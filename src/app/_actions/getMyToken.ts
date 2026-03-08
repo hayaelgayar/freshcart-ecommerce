1bf2b69277a335910186a458b8089ed59ca6c7c4
hayaelgayar <hayaelgayar9@gmail.com>
2026-03-08 03:15:02 +0200

    fresh cart 7

--- src/app/_actions/getMyToken.ts
+++ src/app/_actions/getMyToken.ts
@@ -6,7 +6,7 @@ import { cookies } from "next/headers";
 export async function getMyToken() {
   console.log(process.env.env);
   const myCookies = await cookies();
-  const isLocal = process.env.env;
+  const isLocal = process.env.env=="local";
   const TokenFromCookies = myCookies.get(
     isLocal ? "next-auth.session-token" : "__Secure-next-auth.session-token",
   )?.value;
