bec2a603d49be2cc62408c5b67b1a59ef3448649
hayaelgayar <hayaelgayar9@gmail.com>
2026-03-08 03:11:04 +0200

    fresh cart commit 6

--- src/app/_actions/getMyToken.ts
+++ src/app/_actions/getMyToken.ts
@@ -1,21 +1,24 @@
-"use server"
+"use server";
 
-import { decode } from "next-auth/jwt"
-import { cookies } from "next/headers"
+import { decode } from "next-auth/jwt";
+import { cookies } from "next/headers";
 
 export async function getMyToken() {
-    const myCookies = await cookies()
-     const TokenFromCookies =
-        myCookies.get("__Secure-next-auth.session-token")?.value 
-    console.log((await cookies()).getAll())
-   if (!TokenFromCookies) {
-    return null
-}
-    
-    const decodedToken = await decode({ 
-        token: TokenFromCookies, 
-        secret: process.env.NEXTAUTH_SECRET! 
-    })
-    
-    return decodedToken?.realTokenFromBackEnd
+  console.log(process.env.env);
+  const myCookies = await cookies();
+  const isLocal = process.env.env;
+  const TokenFromCookies = myCookies.get(
+    isLocal ? "next-auth.session-token" : "__Secure-next-auth.session-token",
+  )?.value;
+  console.log((await cookies()).getAll());
+  if (!TokenFromCookies) {
+    return null;
+  }
+
+  const decodedToken = await decode({
+    token: TokenFromCookies,
+    secret: process.env.NEXTAUTH_SECRET!,
+  });
+
+  return decodedToken?.realTokenFromBackEnd;
 }
